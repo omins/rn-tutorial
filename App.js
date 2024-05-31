b060c0e874821ae35cfe431d36ca7dc1c81a59e6
Kevin Minseok Oh <oms3749@gmail.com>
2024-05-31 18:21:56 +0900

    feat: image and emoji picker

--- App.js
+++ App.js
@@ -1,11 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import IconButton from './components/IconButton';
+import CircleButton from './components/CircleButton';
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showOptions, setShowOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
+  const onReset = () => {
+    setShowOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+    alert('Save image');
+  };
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0]?.uri);
+      setShowOptions(true);
+      console.log(result);
+    } else {
+      alert('No image selected');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImage={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+        {pickedEmoji && (
+          <EmojiSticker stickerSource={pickedEmoji} imageSize={40} />
+        )}
+      </View>
+
+      {showOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon={'refresh'} label='Reset' onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon={'save'} label='Save' onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            label={'Choose a photo'}
+            theme={'primary'}
+            onPress={pickImageAsync}
+          />
+          <Button
+            label={'Use this photo'}
+            onPress={() => setShowOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onCloseModal={onModalClose} onSelect={setPickedEmoji} />
+      </EmojiPicker>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +93,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
---
+++ components/EmojiList.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { FlatList, Image, Platform, Pressable, StyleSheet } from 'react-native';
+import emoji1 from '../assets/images/emoji1.png';
+import emoji2 from '../assets/images/emoji2.png';
+import emoji3 from '../assets/images/emoji3.png';
+import emoji4 from '../assets/images/emoji4.png';
+import emoji5 from '../assets/images/emoji5.png';
+import emoji6 from '../assets/images/emoji6.png';
+
+export default function EmojiList({ onCloseModal, onSelect }) {
+  const [emoji] = useState([emoji1, emoji2, emoji3, emoji4, emoji5, emoji6]);
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={Platform.OS === 'web'}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect?.(item);
+            onCloseModal?.();
+          }}
+        >
+          <Image source={item} key={index} style={styles.image} />
+        </Pressable>
+      )}
+    ></FlatList>
+  );
+}
+
+const styles = StyleSheet.create({
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 20,
+  },
+});
