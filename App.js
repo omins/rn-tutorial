766f3573d3a24c191aeb04770369d59bfd277fff
Kevin Minseok Oh <oms3749@gmail.com>
2024-05-31 18:49:42 +0900

    feat: gesture

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CircleButton from './components/CircleButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -50,7 +51,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImage={PlaceholderImage}
@@ -86,7 +87,7 @@ export default function App() {
         <EmojiList onCloseModal={onModalClose} onSelect={setPickedEmoji} />
       </EmojiPicker>
       <StatusBar style='auto' />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
